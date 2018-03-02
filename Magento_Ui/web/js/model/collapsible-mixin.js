7777e06a8573801bf0ec54d40e08106f6ca50204
Riccardo Caroli <rcaroli@webgriffe.com>
2018-03-02 18:12:42 +0100

    js 2 spaces

--- Magento_Ui/web/js/model/collapsible-mixin.js
+++ Magento_Ui/web/js/model/collapsible-mixin.js
@@ -1,52 +1,52 @@
 define([
-    'jquery',
-    'mage/utils/wrapper'
+  'jquery',
+  'mage/utils/wrapper'
 ], function ($, wrapper) {
-    'use strict';
-    return function (collapsible) {
+  'use strict';
+  return function (collapsible) {
 
-        jQuery.widget('mage.collapsible', $['mage']['collapsible'], {
-            _open: function () {
-                // @FIX add .active class to links and buttons
-                if (this.options.openedState) {
-                    this.trigger.find('a, button').addClass(this.options.openedState);
-                }
-                if (this.options.closedState) {
-                    this.trigger.find('a, button').removeClass(this.options.closedState);
-                }
-                // return
-                return this._super();
-            },
-            _close: function () {
-                // @FIX remove .active class to links and buttons
-                if (this.options.openedState) {
-                    this.trigger.find('a, button').removeClass(this.options.openedState);
-                }
-                if (this.options.closedState) {
-                    this.trigger.find('a, button').addClass(this.options.closedState);
-                }
-                // return
-                return this._super();
-            },
-            _refresh: function () {
-                // @FIX add .active class to links and buttons
-                if (this.options.active && !this.options.disabled) {
-                    if (this.options.openedState) {
-                        this.trigger.find('a, button').addClass(this.options.openedState);
-                    }
-                } else if (this.options.disabled) {
-                    this.disable();
-                } else {
-                    if (this.options.closedState) {
-                        this.trigger.find('a, button').addClass(this.options.closedState);
-                    }
-                }
-                // return
-                return this._super();
-            }
-        });
+    jQuery.widget('mage.collapsible', $['mage']['collapsible'], {
+      _open: function () {
+        // @FIX add .active class to links and buttons
+        if (this.options.openedState) {
+          this.trigger.find('a, button').addClass(this.options.openedState);
+        }
+        if (this.options.closedState) {
+          this.trigger.find('a, button').removeClass(this.options.closedState);
+        }
+        // return
+        return this._super();
+      },
+      _close: function () {
+        // @FIX remove .active class to links and buttons
+        if (this.options.openedState) {
+          this.trigger.find('a, button').removeClass(this.options.openedState);
+        }
+        if (this.options.closedState) {
+          this.trigger.find('a, button').addClass(this.options.closedState);
+        }
+        // return
+        return this._super();
+      },
+      _refresh: function () {
+        // @FIX add .active class to links and buttons
+        if (this.options.active && !this.options.disabled) {
+          if (this.options.openedState) {
+            this.trigger.find('a, button').addClass(this.options.openedState);
+          }
+        } else if (this.options.disabled) {
+          this.disable();
+        } else {
+          if (this.options.closedState) {
+            this.trigger.find('a, button').addClass(this.options.closedState);
+          }
+        }
+        // return
+        return this._super();
+      }
+    });
 
-        return $['mage']['collapsible'];
+    return $['mage']['collapsible'];
 
-    };
+  };
 });
--- Magento_Ui/web/js/model/menu-mixin.js
+++ Magento_Ui/web/js/model/menu-mixin.js
@@ -1,32 +1,32 @@
 define([
-    'jquery',
-    'mage/utils/wrapper'
+  'jquery',
+  'mage/utils/wrapper'
 ], function ($, wrapper) {
-    'use strict';
-    return function (menu) {
+  'use strict';
+  return function (menu) {
 
-        jQuery.widget('mage.menu', $['mage']['menu'], {
-            _listen: function () {
-                // @FIX multiple triggers because double mage/menu
-                $(this.controls.toggleBtn).off('click');
-                $(this.controls.swipeArea).off('click');
-                // return
-                return this._super();
-            },
-            _toggleMobileMode: function () {
-                // @FIX add .ui-menu-item.active
-                $(this.element).find('.ui-menu-item').off('click').on('click', function () {
-                    $('.ui-menu-item').removeClass('active');
-                    $('.ui-menu-item').find('> a, > button').removeClass('active');
-                    $(this).addClass('active');
-                    $(this).find('> a, > button').addClass('active');
-                });
-                // return
-                return this._super();
-            }
+    jQuery.widget('mage.menu', $['mage']['menu'], {
+      _listen: function () {
+        // @FIX multiple triggers because double mage/menu
+        $(this.controls.toggleBtn).off('click');
+        $(this.controls.swipeArea).off('click');
+        // return
+        return this._super();
+      },
+      _toggleMobileMode: function () {
+        // @FIX add .ui-menu-item.active
+        $(this.element).find('.ui-menu-item').off('click').on('click', function () {
+          $('.ui-menu-item').removeClass('active');
+          $('.ui-menu-item').find('> a, > button').removeClass('active');
+          $(this).addClass('active');
+          $(this).find('> a, > button').addClass('active');
         });
+        // return
+        return this._super();
+      }
+    });
 
-        return $['mage']['menu'];
+    return $['mage']['menu'];
 
-    };
+  };
 });
