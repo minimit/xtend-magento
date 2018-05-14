8d53885c7b9c062f245b382ab9c95f8a3632d0e9
Riccardo Caroli <rcaroli@webgriffe.com>
2018-05-14 18:35:10 +0200

    xtend v0.4.13

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 'use strict';
 
+var fs = require('fs');
 var gulp = require('gulp');
 var buffer = require('vinyl-buffer');
 var source = require('vinyl-source-stream');
 
 var less = require('gulp-less');
 var watch = require('gulp-watch');
+var replace = require('gulp-replace');
 var browserify = require('browserify');
 var cleanCSS = require('gulp-clean-css');
 var sourcemaps = require('gulp-sourcemaps');
@@ -14,7 +16,10 @@ var uglify = require('gulp-uglify-es').default;
 // compile less
 
 gulp.task('less', function () {
+  const version = JSON.parse(fs.readFileSync('package.json')).version;
+  var banner = "/*! xtend-magento v" + version + " (https://getxtend.com/)\n" + "@copyright (c) 2017 - 2018 Riccardo Caroli\n" + "@license MIT (https://github.com/minimit/xtend-library/blob/master/LICENSE) */";
   return gulp.src(['web/**/theme.less'])
+    .pipe(replace(/\/\*\![^\*]+\*\//, banner))
     .pipe(sourcemaps.init())
     .pipe(less())
     .pipe(cleanCSS())
@@ -33,8 +38,11 @@ gulp.task('js', function () {
     entries: 'web/js/xtend-magento.js',
     debug: true
   });
+  const version = JSON.parse(fs.readFileSync('package.json')).version;
+  var banner = "/*! xtend-magento v" + version + " (https://getxtend.com/)\n" + "@copyright (c) 2017 - 2018 Riccardo Caroli\n" + "@license MIT (https://github.com/minimit/xtend-library/blob/master/LICENSE) */";
   return b.bundle()
     .pipe(source('xtend-magento.min.js'))
+    .pipe(replace(/\/\*\![^\*]+\*\//, banner))
     .pipe(buffer())
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(uglify({
